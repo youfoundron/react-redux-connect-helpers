3bf73659da5e9f31ed7d9a566389fc1588a3df66
Ron Gierlach <rongierlach@gmail.com>
2017-01-14 20:43:22 -0600

    webpack config

---
+++ webpack.config.babel.js
@@ -0,0 +1,37 @@
+import { resolve } from 'path'
+
+const makeRule = (ext, loader, enforce, exclude, include) => ({
+  test: typeof ext === 'string'
+    ? RegExp(`/.${ext}$/`)
+    : ext,
+  use: typeof loader === 'string'
+    ? loader
+    : loader || `${ext}-loader`,
+  enforce:
+    enforce || undefined,
+  exclude:
+    exclude || /node_modules/,
+  include:
+    include || undefined
+})
+
+export default {
+  entry: resolve(__dirname, './src/index.js'),
+  output: {
+    path: resolve(__dirname, './lib'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      makeRule(/\.jsx?$/, 'standard-loader', 'pre'),
+      makeRule(/\.jsx?$/, 'babel-loader')
+    ]
+  },
+  stats: {
+    chunks: true,
+    colors: true,
+    hash: true,
+    version: false,
+    timings: true
+  }
+}

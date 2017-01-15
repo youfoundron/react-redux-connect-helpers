940e0350154546cae84457216bfd8c6fd45179af
Ron Gierlach <rongierlach@gmail.com>
2017-01-14 20:42:40 -0600

    src named exports

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/* @flow */
+import connectValue from './connectValue'
+import connectStateValue from './connectStateValue'
+import createActionConnector from './createActionConnector'
+
+export {
+  connectValue,
+  connectStateValue,
+  createActionConnector
+}

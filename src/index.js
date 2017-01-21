40996f78c4167a3cb3f09b1683f8f505edcbab90
Ron Gierlach <rongierlach@gmail.com>
2017-01-20 21:27:30 -0600

    export a default module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import connectValue from './connectValue'
 import connectStateValue from './connectStateValue'
 import createActionConnector from './createActionConnector'
 
+export default {
+  connectValue,
+  connectStateValue,
+  createActionConnector
+}
+
 export {
   connectValue,
   connectStateValue,

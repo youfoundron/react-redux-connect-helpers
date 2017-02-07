cabfd8c8f73b8fe06c79f8c4ecf806521c8fb6b1
Ron Gierlach <rongierlach@gmail.com>
2017-02-07 16:43:05 -0600

    immutable export

---
+++ src/immutable/index.js
@@ -0,0 +1,18 @@
+import util from './util'
+import connectValue from '../connectValue'
+import createActionConnector from '../createActionConnector'
+import connectStateValueFactory from '../connectStateValueFactory'
+
+const connectStateValue = connectStateValueFactory(util)
+
+export default {
+  connectValue,
+  connectStateValue,
+  createActionConnector
+}
+
+export {
+  connectValue,
+  connectStateValue,
+  createActionConnector
+}

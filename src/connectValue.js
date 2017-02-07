d3eb0c591e75785cf9b196a13e82dfc444cda762
Ron Gierlach <rongierlach@gmail.com>
2017-02-07 16:05:09 -0600

    refactor and remove dependency on connectStateValue

--- src/connectValue.js
+++ src/connectValue.js
@@ -1,10 +1,12 @@
-import connectStateValue from './connectStateValue'
-import { always } from 'ramda'
+import { connect } from 'react-redux'
 
 /**
  * Returns a function that connects a value to a React component as a prop
  * @param {any} value
  * @param {string} propName
  */
-export default (value, propName) =>
-  connectStateValue(null, propName, always(value))
+export default (value, propName) => connect(
+  state => ({
+    [propName]: value
+  })
+)

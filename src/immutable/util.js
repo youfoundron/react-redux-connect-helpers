c8c62240acf0cfe712e6623149fc35f7a0a9cd90
Ron Gierlach <rongierlach@gmail.com>
2017-02-07 16:42:52 -0600

    utils for immutable variant

---
+++ src/immutable/util.js
@@ -0,0 +1,36 @@
+import {
+  ifElse,
+  isArrayLike,
+  last,
+  identity
+} from 'ramda'
+
+/**
+ * Takes any number of arguments and puts them in an array
+ */
+const toArray = (...args) => [...args]
+
+/**
+ * Returns the given path array, handles string values
+ * @param {Array.<string>|string} pathArray
+ */
+const normalizePathArray = ifElse(isArrayLike, identity, toArray)
+
+/**
+ * Returns the value at the end of the path for the given Immutable Map
+ * @param {Array.<string>|string} pathArray
+ * @param {Immutable Structure} immutable
+ */
+const getValueFromPathArray = (pathArray, immutable) =>
+  immutable.getIn(normalizePathArray(pathArray))
+
+/**
+ * Returns the property name inferred from the given path array
+ * @param {Array.<string>|string} pathArray
+ */
+const getPropNameFromPathArray = ifElse(isArrayLike, last, identity)
+
+export default {
+  getValueFromPathArray,
+  getPropNameFromPathArray
+}
